79cfba7f3b1f53554e0cf81fa8f35aa768ee1aed
idimitrijevic9920rn <idimitrijevic5519ri@raf.rs>
2021-11-03 01:42:20 +0100

    Initial commit

---
+++ src/main/java/specification/ProjectSpecification.java
@@ -0,0 +1,12 @@
+package specification;
+
+public interface ProjectSpecification {
+
+    public abstract void saveFile();
+    public abstract void downloadFile();
+    public abstract void deleteFile();
+    public abstract void viewFile();
+    public abstract void createStorage();
+    public abstract void moveFile();
+    
+}
