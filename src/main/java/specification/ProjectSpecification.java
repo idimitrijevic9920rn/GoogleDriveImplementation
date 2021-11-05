aebc34ea0cde9845dd297d9b2ed27b6f110fde2f
idimitrijevic9920rn <idimitrijevic5519ri@raf.rs>
2021-11-05 21:44:03 +0100

    update

--- src/main/java/specification/ProjectSpecification.java
+++ src/main/java/specification/ProjectSpecification.java
@@ -1,12 +1,15 @@
 package specification;
 
+import java.io.IOException;
+
 public interface ProjectSpecification {
 
     public abstract void saveFile();
     public abstract void downloadFile();
-    public abstract void deleteFile();
+    public abstract void deleteFile(String path, int val);
     public abstract void viewFile();
-    public abstract void createStorage();
-    public abstract void moveFile();
+    public abstract void createStorage(String path,int val);
+    public abstract void createFile(String path,int val);
+    public abstract void moveFile(String file,String target);
     
 }
