7ee39d2cbef7fc41c67ff1d679d15a5e07f0a7ab
idimitrijevic9920rn <idimitrijevic5519ri@raf.rs>
2021-11-09 22:51:53 +0100

    dodata metoda za login

--- src/main/java/specification/ProjectSpecification.java
+++ src/main/java/specification/ProjectSpecification.java
@@ -5,11 +5,12 @@ import java.io.IOException;
 public interface ProjectSpecification {
 
     public abstract void saveFile();
-    public abstract void downloadFile();
+    public abstract void downloadFile(String path);
     public abstract void deleteFile(String path, int val);
-    public abstract void viewFile();
+    public abstract void viewFile(String files);
     public abstract void createStorage(String path,int val);
     public abstract void createFile(String path,int val);
     public abstract void moveFile(String file,String target);
+    public abstract void logIn(String username,String password);
     
 }
